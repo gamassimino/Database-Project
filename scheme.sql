5ff4974204c545e496e355b989f0d5d93102dc74
ezequiel <ezedepetris@gmail.com>
2015-05-15 09:02:27 -0300

    with out minuscule

--- scheme.sql
+++ scheme.sql
@@ -1,42 +1,38 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    email VARCHAR(60) UNIQUE,
-    first_name VARCHAR(56),
-    last_name VARCHAR(56),
-  CONSTRAINT users_pk PRIMARY KEY (id)
+drop table if exists users;
+create table users(
+    id int(11) not null auto_increment,
+    email varchar(60) unique,
+    first_name varchar(56),
+    last_name varchar(56),
+  constraint users_pk primary key (id)
 );
 
 
-DROP TABLE IF EXISTS grids;
-CREATE TABLE grids(
-	codigo integer NOT NULL,
+drop table if exists grids;
+create table grids(
+	codigo integer not null unique,
 	fecha date,
 	hora_inicio varchar(50),
 	hora_fin varchar(50),
 	ganador varchar(50),
-	email varchar(50) NOT NULL,
-	FOREIGN KEY (email) REFERENCES Usuario (email),
-	PRIMARY KEY (codigo)
+	email varchar(50) not null,
+	constraint codigo_pk primary key (codigo)
 );
 
-DROP TABLE IF EXISTS cells;
-CREATE TABLE cells(
-	pos_x integer NOT NULL,
-	pos_y integer NOT NULL,
-	PRIMARY KEY (pos_x,pos_y)
+drop table if exists cells;
+create table cells(
+	pos_x integer not null,
+	pos_y integer not null,
+	constraint pos_pk primary key (pos_x,pos_y)
 );
 
-DROP TABLE IF EXISTS compuesta;
-CREATE TABLE compuesta(
-	pos_x integer NOT NULL,
-	pos_y integer NOT NULL,
-	codigo integer NOT NULL,
-	orden_jugada varchar(50) NOT NULL,
-	FOREIGN KEY (pos_x) REFERENCES Celda (pos_x),
-	FOREIGN KEY (pos_y) REFERENCES Celda (pos_y),
-	FOREIGN KEY (codigo) REFERENCES Partida (codigo),
-	PRIMARY KEY (pos_x,pos_y,codigo)
+drop table if exists composed;
+create table composed(
+	pos_x integer not null,
+	pos_y integer not null,
+	codigo integer not null,
+	orden_jugada varchar(50) not null,
+	constraint ps_codigo_pk primary key (pos_x,pos_y,codigo)
 );
 
 
