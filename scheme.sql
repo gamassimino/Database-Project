99e0482a7d6dc8110f3212fe1c779731fcf0facc
gamassimino <gamassimino01@gmail.com>
2015-05-21 21:45:19 -0300

    new scheme

--- scheme.sql
+++ scheme.sql
@@ -1,38 +1,60 @@
+drop database connect4_chino;
+create database if not exists connect4_chino;
+	use connect4_chino;
+
 drop table if exists users;
 create table users(
-    id int(11) not null auto_increment,
-    email varchar(60) unique,
-    first_name varchar(56),
-    last_name varchar(56),
-  constraint users_pk primary key (id)
-);
+ email varchar(50) NOT NULL,
+ first_name varchar(50),
+ id varchar(50),
+ last_name varchar(50),
+PRIMARY KEY (email));
 
+drop table if exists deleteUsers;
+create table deleteUsers(
+ email varchar(50) NOT NULL,
+ first_name varchar(50),
+ id varchar(50),
+ last_name varchar(50),
+PRIMARY KEY (email));
 
-drop table if exists grids;
-create table grids(
-	codigo integer not null unique,
-	fecha date,
-	hora_inicio varchar(50),
-	hora_fin varchar(50),
-	ganador varchar(50),
-	email varchar(50) not null,
-	constraint codigo_pk primary key (codigo)
+drop table if exists games;
+create table games(
+ code integer NOT NULL,
+ aDate date,
+ hour_begin datetime,
+ hour_end datetime,
+ size varchar(50),-- CAMBIAR POR LAS DIMENSIONES
+ result varchar(50),
+ email varchar(50) NOT NULL,
+ FOREIGN KEY (email) REFERENCES users (email),
+ PRIMARY KEY (code)
 );
 
 drop table if exists cells;
-create table cells(
-	pos_x integer not null,
-	pos_y integer not null,
-	constraint pos_pk primary key (pos_x,pos_y)
-);
+CREATE TABLE cells(
+ pos_x integer NOT NULL,
+ pos_y integer NOT NULL,
+PRIMARY KEY (pos_x,pos_y));
 
 drop table if exists composed;
-create table composed(
-	pos_x integer not null,
-	pos_y integer not null,
-	codigo integer not null,
-	orden_jugada varchar(50) not null,
-	constraint ps_codigo_pk primary key (pos_x,pos_y,codigo)
+CREATE TABLE composed(
+ pos_x integer NOT NULL,
+ pos_y integer NOT NULL,
+ code integer NOT NULL,
+ play_order varchar(50) NOT NULL,
+ PRIMARY KEY (code),
+ FOREIGN KEY (pos_x,pos_y) REFERENCES cells (pos_x,pos_y),
+ FOREIGN KEY (code) REFERENCES games (code)
 );
+
+
+
+
+
+
+
+
+
 
 
