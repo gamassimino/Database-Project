095a3e74633d30dd4c155b5a009a5be07c02c073
gamassimino <gamassimino01@gmail.com>
2015-05-27 17:31:58 -0300

    second date

--- scheme.sql
+++ scheme.sql
@@ -4,48 +4,48 @@ create database if not exists connect4_chino;
 
 drop table if exists users;
 create table users(
- email varchar(50) NOT NULL,
+ email varchar(50) not null,
  first_name varchar(50),
  id varchar(50),
  last_name varchar(50),
-PRIMARY KEY (email));
+primary key (email));
 
 drop table if exists deleteUsers;
 create table deleteUsers(
- email varchar(50) NOT NULL,
+ email varchar(50) not null,
  first_name varchar(50),
  id varchar(50),
  last_name varchar(50),
-PRIMARY KEY (email));
+primary key (email));
 
 drop table if exists games;
 create table games(
- code integer NOT NULL,
+ code integer not null,
  aDate date,
  hour_begin datetime,
  hour_end datetime,
  size varchar(50),-- CAMBIAR POR LAS DIMENSIONES
  result varchar(50),
- email varchar(50) NOT NULL,
- FOREIGN KEY (email) REFERENCES users (email),
- PRIMARY KEY (code)
+ email varchar(50) not null,
+ foreign key (email) references users (email),
+ primary key (code)
 );
 
 drop table if exists cells;
-CREATE TABLE cells(
- pos_x integer NOT NULL,
- pos_y integer NOT NULL,
-PRIMARY KEY (pos_x,pos_y));
+create table cells(
+ pos_x integer not null,
+ pos_y integer not null,
+primary key (pos_x,pos_y));
 
 drop table if exists composed;
-CREATE TABLE composed(
- pos_x integer NOT NULL,
- pos_y integer NOT NULL,
- code integer NOT NULL,
- play_order varchar(50) NOT NULL,
- PRIMARY KEY (code),
- FOREIGN KEY (pos_x,pos_y) REFERENCES cells (pos_x,pos_y),
- FOREIGN KEY (code) REFERENCES games (code)
+create table composed(
+ pos_x integer not null,
+ pos_y integer not null,
+ code integer not null,
+ play_order varchar(50) not null,
+ primary key (code),
+ foreign key (pos_x,pos_y) references cells (pos_x,pos_y),
+ foreign key (code) references games (code)
 );
 
 
