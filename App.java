0443a0b7767a478cced6783f954091ac1b5046b5
gamassimino <gamassimino01@gmail.com>
2015-06-09 18:38:33 -0300

    ussing dictionary

--- App.java
+++ App.java
@@ -5,53 +5,62 @@ public class App{
   public static void main(String[] args) {
 
     try {
-      String driver = "org.postgresql.Driver";
-      String url = "jdbc:postgresql://localhost:5432/connec4";
-      String username = "postgres";
-      String password = "root";
+		String driver = "org.postgresql.Driver";
+		String url = "jdbc:postgresql://localhost:5432/connec4";
+		String username = "postgres";
+		String password = "root";
 
-      // Load database driver if not already loaded.
-      Class.forName(driver);
-      // Establish network connection to database.
-      Connection connection = DriverManager.getConnection(url, username, password);
+		// Load database driver if not already loaded.
+		Class.forName(driver);
+		// Establish network connection to database.
+		Connection connection = DriverManager.getConnection(url, username, password);
 
-      String query = "set search_path = connect4_chino;";
-      PreparedStatement statement = connection.prepareStatement(query);
-      statement.execute();
+		String query = "set search_path = connect4_chino;";
+		PreparedStatement statement = connection.prepareStatement(query);
+		statement.execute();
+
+/*********************************GET VALUES***********************************************/
+		String email = "samuelitox@gmail.com";
+		String first_name = "samuel";
+		String last_name = "samuelo";
+		Dictionary dictionary = new Dictionary();
+/******************************************************************************************/
 
 /*********************************INSERT USER**********************************************/
-      query = "insert into users values ('gamas@gmail.com','gas','mass')";
-      statement = connection.prepareStatement(query);
-      int reg = statement.executeUpdate();
+    //query = "insert into users values('"+email+"','"+first_name+"','"+last_name+"')";
+	query = dictionary.insertUser(email,first_name,last_name);
+    statement = connection.prepareStatement(query);
+    int reg = statement.executeUpdate();
 
-      query = "select * from users";
-      statement = connection.prepareStatement(query);
-      ResultSet resultSet = statement.executeQuery();
-      // while(resultSet.next()){
-      //   System.out.println(resultSet.getString("email"));
-      // 	System.out.println(resultSet.getString("first_name"));
-      // 	System.out.println(resultSet.getString("last_name"));
-      // }
+    query = "select * from users";
+    statement = connection.prepareStatement(query);
+    ResultSet resultSet = statement.executeQuery();
+    while(resultSet.next()){
+        System.out.println(resultSet.getString("email"));
+      	System.out.println(resultSet.getString("first_name"));
+      	System.out.println(resultSet.getString("last_name"));
+    }
 /******************************************************************************************/
 
 /*******************************DELETE USER************************************************/
- 	 query = "delete from users where (email = 'gamas@gmail.com')";
-      statement = connection.prepareStatement(query);
-      reg = statement.executeUpdate();
+ 	// query = "delete from users where (email = 'gamas@gmail.com')";
+  //   statement = connection.prepareStatement(query);
+  //   reg = statement.executeUpdate();
 
-      query = "select * from deleteUsers";
-      statement = connection.prepareStatement(query);
-      resultSet = statement.executeQuery();
-      while(resultSet.next()){
-      	System.out.println(resultSet.getString("email"));
-      	System.out.println(resultSet.getString("first_name"));
-      	System.out.println(resultSet.getString("last_name"));
-      	System.out.println(resultSet.getString("admin"));
-      	System.out.println(resultSet.getString("delete_date"));
-      }
+  //   query = "select * from deleteUsers";
+  //   statement = connection.prepareStatement(query);
+  //   resultSet = statement.executeQuery();
+  //   while(resultSet.next()){
+  //     	System.out.println(resultSet.getString("email"));
+  //     	System.out.println(resultSet.getString("first_name"));
+  //     	System.out.println(resultSet.getString("last_name"));
+  //     	System.out.println(resultSet.getString("admin"));
+  //     	System.out.println(resultSet.getString("delete_date"));
+  //   }
 /******************************************************************************************/
 
 /*******************************SHOW GAMES*************************************************/
+
 /******************************************************************************************/
     
     } catch(ClassNotFoundException cnfe) {
--- Dictionary.java
+++ Dictionary.java
@@ -1,16 +1,16 @@
 public class Dictionary{
 
 	public String insertUser(String email, String first_name, String last_name){
-		return "insert into users values("+email+","+first_name+","+last_name+")";
+		return "insert into users values('"+email+"','"+first_name+"','"+last_name+"')";
 	}
 
 	public String deleteUser(String email, String first_name, String last_name){
-		return "insert into users values("+email+","+first_name+","+last_name+","+current_user+","+now()+")";
+		return "insert into users values('"+email+"','"+first_name+"','"+last_name+"',current_user,now())";
 	}
 
 	public String showGames(String email){
-		String p1 = "select palyer1,code,date_begin,date_end,hour_begin,hour_end games where (player2 = "+email+");";
-		String p2 = "select player2,code,date_begin,date_end,hour_begin,hour_end games where (palyer1 = "+email+")";
+		String p1 = "select palyer1,code,date_begin,date_end,hour_begin,hour_end games where (player2 = '"+email+"'); ";
+		String p2 = "select player2,code,date_begin,date_end,hour_begin,hour_end games where (palyer1 = '"+email+"')";
 		return p1+p2; 
 	}
 }
