4e7d7c7d04811a20bff28cd523784ba9892226a8
gamassimino <gamassimino01@gmail.com>
2015-06-09 17:34:42 -0300

    point 5

--- App.java
+++ App.java
@@ -6,7 +6,7 @@ public class App{
 
     try {
       String driver = "org.postgresql.Driver";
-      String url = "jdbc:postgresql://localhost:5432/postgres";
+      String url = "jdbc:postgresql://localhost:5432/connec4";
       String username = "postgres";
       String password = "root";
 
@@ -15,14 +15,49 @@ public class App{
       // Establish network connection to database.
       Connection connection = DriverManager.getConnection(url, username, password);
 
-      String query = "SELECT * FROM persona ";
+      String query = "set search_path = connect4_chino;";
       PreparedStatement statement = connection.prepareStatement(query);
-      ResultSet resultSet = statement.executeQuery();
+      statement.execute();
 
-    }
+/*********************************INSERT USER**********************************************/
+      query = "insert into users values ('gamas@gmail.com','gas','mass')";
+      statement = connection.prepareStatement(query);
+      int reg = statement.executeUpdate();
+
+      query = "select * from users";
+      statement = connection.prepareStatement(query);
+      ResultSet resultSet = statement.executeQuery();
+      // while(resultSet.next()){
+      //   System.out.println(resultSet.getString("email"));
+      // 	System.out.println(resultSet.getString("first_name"));
+      // 	System.out.println(resultSet.getString("last_name"));
+      // }
+/******************************************************************************************/
+
+/*******************************DELETE USER************************************************/
+ 	 query = "delete from users where (email = 'gamas@gmail.com')";
+      statement = connection.prepareStatement(query);
+      reg = statement.executeUpdate();
+
+      query = "select * from deleteUsers";
+      statement = connection.prepareStatement(query);
+      resultSet = statement.executeQuery();
+      while(resultSet.next()){
+      	System.out.println(resultSet.getString("email"));
+      	System.out.println(resultSet.getString("first_name"));
+      	System.out.println(resultSet.getString("last_name"));
+      	System.out.println(resultSet.getString("admin"));
+      	System.out.println(resultSet.getString("delete_date"));
+      }
+/******************************************************************************************/
+
+/*******************************SHOW GAMES*************************************************/
+/******************************************************************************************/
+    
     } catch(ClassNotFoundException cnfe) {
       System.err.println("Error loading driver: " + cnfe);
-    } catch(SQLException sqle) {
+    } 
+    catch(SQLException sqle) {
     	sqle.printStackTrace();
       System.err.println("Error connecting: " + sqle);
     }
